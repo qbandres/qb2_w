4f00d66c7cb2f76ff41f22ee5c161e6341016f5b
Andres <qbandres@gmail.com>
2021-08-12 01:39:45 -0400

    Don't worry

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from os import closerange
+import pandas as pd
+import numpy as np
 
+A = [3,0.45443,6.23423]
+B=[5,6.343,np.NaN]
 
-s="lamina"
-print(s[::-1])
+df= pd.DataFrame(list(zip(A,B)),columns=['A','B'])
+
+print(df)
+
+df['C']=df.A*df.B
+
+print(df)
